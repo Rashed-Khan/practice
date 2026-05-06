6c51dd41d2eb3b728c501290f0124fb420247730
Rashed-Khan <eng.rashedmilon002@gmail.com>
2026-05-06 17:14:24 +0600

    profile ui 1 final

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class ProfileUI extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Milon Khan'),
+        title: const Text("Profile"),
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
@@ -34,57 +34,65 @@ class ProfileUI extends StatelessWidget {
             borderRadius: BorderRadius.circular(20),
           ),
           child: Padding(
-            padding: EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                CircleAvatar(
-                  radius: 55,
-                  backgroundColor: Colors.blue,
-                  child: CircleAvatar(
-                    radius: 50,
-                    backgroundImage: NetworkImage(
-                      'https://images.pexels.com/photos/33106773/pexels-photo-33106773.jpeg',
-                    ),
+                const CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage(
+                    "https://images.pexels.com/photos/33106773/pexels-photo-33106773/free-photo-of-elderly-man-relaxing-outdoors-by-stone-wall-in-izmir.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
                   ),
                 ),
 
-                SizedBox(height: 15,),
-                Text('Milon khan',style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),),
-                SizedBox(height: 8,),
-                Row(
+                const SizedBox(height: 15),
+
+                const Text(
+                  "Elderly Man",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                const SizedBox(height: 8),
+
+                const Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
                   children: [
-
-                    Icon(Icons.email,color: Colors.grey,),
-                    SizedBox(width: 8,),
-                    Text('milon@gmail.com',style: TextStyle(color: Colors.red),),
-
+                    Icon(Icons.email, color: Colors.grey),
+                    SizedBox(width: 8),
+                    Text(
+                      "example@email.com",
+                      style: TextStyle(color: Colors.grey),
+                    ),
                   ],
                 ),
-                SizedBox(height: 20,),
-                ElevatedButton(onPressed: (){},
+
+                const SizedBox(height: 20),
+
+                ElevatedButton(
+                  onPressed: () {},
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.blue,
-                    padding: EdgeInsets.symmetric(
+                    padding: const EdgeInsets.symmetric(
                       horizontal: 40,
                       vertical: 12,
-
                     ),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(12),
-                    )
+                    ),
                   ),
-
-                    child: Text('Follow',style: TextStyle(fontSize: 16),),),
-
+                  child: const Text(
+                    "Follow",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
               ],
             ),
           ),
         ),
       ),
     );
-
   }
 }
